ca3823f471cf7ddc63fc6dd09d3080a3f41b5ff7
ovoloshchuk <ovoloshchuk@Scout.lomspace.local>
2011-09-12 17:08:23 +0300

    Trying to make decoding-reencoding work.

--- src/tagencoder/main/SongRecodeActivity.java
+++ src/tagencoder/main/SongRecodeActivity.java
@@ -13,8 +13,20 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore.Audio.AudioColumns;
 import android.provider.MediaStore.Audio.Media;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -52,15 +64,42 @@ public class SongRecodeActivity extends Activity {
         artist = c.getString(c.getColumnIndex(AudioColumns.ARTIST));
         c.close();
         fillData(title, album, artist);
+        
+        ArrayAdapter<CharSequence> encodingAdapter = 
+                ArrayAdapter.createFromResource(this, R.array.encodings, android.R.layout.simple_spinner_item);
+        Spinner spinner = (Spinner)findViewById(R.id.Encoding);
+        spinner.setAdapter(encodingAdapter);        
     }
     
     public void fillData(String title, String album, String artist) {
         EditText etTitle = (EditText)findViewById(R.id.Title);
         EditText etAlbum = (EditText)findViewById(R.id.Album);
         EditText etArtist = (EditText)findViewById(R.id.Artist);
-        
-        etTitle.setText(title);
-        etAlbum.setText(album);
-        etArtist.setText(artist);
+        try {
+            etTitle.setText(decode(title, "windows-1251"));
+            etAlbum.setText(decode(album, "Cp1251"));
+            etArtist.setText(decode(artist, "utf-8"));
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(SongRecodeActivity.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private String decode(String input, String charsetName) throws UnsupportedEncodingException {        
+        Charset charset = Charset.forName(charsetName);
+        CharsetDecoder decoder = charset.newDecoder();
+        byte[] in = input.getBytes(charsetName);
+        byte[] out = new byte[in.length/2];
+        for (int i = 1; i < in.length; i+=2) {
+            out[i/2] = in[i];
+        }
+        ByteBuffer bbuf = ByteBuffer.wrap(out);
+        String sReturn = null;
+        try {
+            CharBuffer cbuf = decoder.decode(bbuf);
+            sReturn = cbuf.toString();
+        } catch (CharacterCodingException ex) {
+            Logger.getLogger(SongRecodeActivity.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sReturn;
     }
 }
