44b18d570ebd9797cbd96dc4c6b477f882a1abe5
Orest Voloshchuk <orest.v@gmail.com>
2011-09-20 00:02:49 +0300

    Encoding does work now

--- src/tagencoder/main/SongRecodeActivity.java
+++ src/tagencoder/main/SongRecodeActivity.java
@@ -6,34 +6,19 @@ package tagencoder.main;
 
 import TagEncoderLib.BicycleTagEncoder;
 import android.app.Activity;
-import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore.Audio.AudioColumns;
-import android.provider.MediaStore.Audio.Media;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.CharacterCodingException;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CharsetEncoder;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -42,9 +27,9 @@ import java.util.logging.Logger;
  *
  * @author seth
  */
-public class SongRecodeActivity extends Activity implements OnItemClickListener{
+public class SongRecodeActivity extends Activity implements OnItemSelectedListener{
 
-    private long nSongId = 0;
+    private Uri songUri;
 
     /** Called when the activity is first created. */
     @Override
@@ -52,15 +37,22 @@ public class SongRecodeActivity extends Activity implements OnItemClickListener{
         super.onCreate(icicle);
         setContentView(R.layout.songedit);
         Intent i = this.getIntent();
-        Uri uri = i.getData();
-        this.nSongId = ContentUris.parseId(uri);
+        this.songUri = i.getData();
 
+        ArrayAdapter<CharSequence> encodingAdapter =
+                ArrayAdapter.createFromResource(this, R.array.encodings, android.R.layout.simple_spinner_item);
+        Spinner spinner = (Spinner) findViewById(R.id.Encoding);
+        spinner.setAdapter(encodingAdapter);
+        spinner.setOnItemSelectedListener(this);        
+    }
+    
+    private void setSongData(Uri uri, String sCharset) {
         String title = "", album = "", artist = "";
         HashMap<String, String> hmTags = null;
         FileInputStream fis = null;
         try {
             fis = (FileInputStream) getContentResolver().openInputStream(uri);
-            hmTags = BicycleTagEncoder.getTags(fis, "CP1251");
+            hmTags = BicycleTagEncoder.getTags(fis, sCharset);
             fis.close();
         } catch (IOException ex) {
             Logger.getLogger(SongRecodeActivity.class.getName()).log(Level.SEVERE, null, ex);
@@ -69,13 +61,6 @@ public class SongRecodeActivity extends Activity implements OnItemClickListener{
         album = hmTags.get("TALB");
         artist = hmTags.get("TPE1");
         fillData(title, album, artist);
-
-        ArrayAdapter<CharSequence> encodingAdapter =
-                ArrayAdapter.createFromResource(this, R.array.encodings, android.R.layout.simple_spinner_item);
-        Spinner spinner = (Spinner) findViewById(R.id.Encoding);
-        spinner.setAdapter(encodingAdapter);
-        spinner.setOnItemClickListener(this);
-        
     }
 
     public void fillData(String title, String album, String artist) {
@@ -87,8 +72,12 @@ public class SongRecodeActivity extends Activity implements OnItemClickListener{
         etArtist.setText(artist);
     }
 
-    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-        String s = (String) arg0.getAdapter().getItem(arg2);
-        System.out.println(s);
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+        String sCharset = (String) arg0.getAdapter().getItem(arg2);
+        setSongData(this.songUri, sCharset);
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
