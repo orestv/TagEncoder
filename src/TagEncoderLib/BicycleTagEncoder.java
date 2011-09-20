b1235a6002af3924afae20436d0e32d7f5a58b32
ovoloshchuk <ovoloshchuk@Scout.lomspace.local>
2011-09-20 10:48:42 +0300

    Refactored and commented BicycleTagEncoder.

--- src/TagEncoderLib/BicycleTagEncoder.java
+++ src/TagEncoderLib/BicycleTagEncoder.java
@@ -30,57 +30,56 @@ public class BicycleTagEncoder {
         return nResult;
     }
 
-    private static HashMap<String, String> getTags(byte[] baTags, String sCharsetName) {
+    private static HashMap<String, String> getTags(byte[] baTags, String sCharsetName) throws IOException {
         HashMap<String, String> hmResult = new HashMap<String, String>();
 
         DataInputStream dis = new DataInputStream(new ByteArrayInputStream(baTags));
         while (true) {
-            try {
-                if (dis.available() == 0) {
-                    break;
-                }
-                byte[] baTagName = new byte[4];
-                byte[] baTagLength = new byte[4];
-                dis.read(baTagName);
-                dis.read(baTagLength);
-                dis.skip(3);
-                int nTagLength = getSynchronizedIntegerValue(baTagLength) - 1;
-                if (nTagLength < 0) {
-                    break;
-                }
-                byte[] baTagValue = new byte[nTagLength];
-                dis.read(baTagValue);
-
-                String sName, sValue;
-                sName = new String(baTagName);
-                sValue = new String(baTagValue, sCharsetName);
-                hmResult.put(sName, sValue);
-
-            } catch (IOException ex) {
-                System.out.println("asdfasdfasdf");
+            if (dis.available() == 0) {
+                break;
             }
+            byte[] baTagName = new byte[4];
+            byte[] baTagLength = new byte[4];
+            dis.read(baTagName);
+            dis.read(baTagLength);
+
+            //Technical value - some flags and encoding specification
+            dis.skip(3);
+            int nTagLength = getSynchronizedIntegerValue(baTagLength) - 1;
+            if (nTagLength <= 0) {
+                break;
+            }
+            byte[] baTagValue = new byte[nTagLength];
+            dis.read(baTagValue);
+
+            String sName, sValue;
+            sName = new String(baTagName);
+            sValue = new String(baTagValue, sCharsetName);
+            hmResult.put(sName, sValue);
         }
 
         return hmResult;
     }
+    
+    private static byte[] getHeaderBytes(InputStream is) throws IOException {
+        //Get ID3 header - 10 bytes
+        byte[] baHeader = new byte[10];
+        is.read(baHeader);
+        
+        //Get last 4 bytes of the header - ID3 header length.
+        DataInputStream dis = new DataInputStream(new ByteArrayInputStream(baHeader, 6, 4));
+        byte[] baHeaderLength = new byte[4];
+        dis.read(baHeaderLength);
+        //Parse synchronized int.
+        int nHeaderLength = BicycleTagEncoder.getSynchronizedIntegerValue(baHeaderLength);
+        //Read the whole header.
+        byte[] baTags = new byte[nHeaderLength];
+        is.read(baTags);
+        return baTags;
+    }
 
-    public static HashMap<String, String> getTags(InputStream fis, String sCharsetName) {
-        try {
-
-            byte[] baHeader = new byte[10];
-            fis.read(baHeader);
-            DataInputStream dis = new DataInputStream(new ByteArrayInputStream(baHeader, 6, 4));
-            byte[] baHeaderLength = new byte[4];
-            dis.read(baHeaderLength);
-            int nHeaderLength = BicycleTagEncoder.getSynchronizedIntegerValue(baHeaderLength);
-            byte[] baTags = new byte[nHeaderLength];
-            fis.read(baTags);
-
-            return BicycleTagEncoder.getTags(baTags, sCharsetName);
-
-        } catch (IOException ex) {
-            Logger.getLogger(BicycleTagEncoder.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
+    public static HashMap<String, String> getTags(InputStream is, String sCharsetName) throws IOException {
+        byte[] baTags = getHeaderBytes(is);
+        return BicycleTagEncoder.getTags(baTags, sCharsetName);
     }
 }
